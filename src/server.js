d32f06997f9d6acac317670452ea750e536cfe0d
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-14 16:58:24 +0200

    stuck

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import  express  from "express";
+import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./services/authors/index.js";
 
+
 const server = express()
+
+
+ server.use(express.json())
 const port = 3001
 // then i have to declare the endpoints
 server.use("/posts", authorsRouter) // same prefix
-server.listen(3001, () => {
+console.table(listEndpoints(server))
+server.listen(port, () => {
     console.log(`my first running server on port ${port}`)
 })
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -4,24 +4,51 @@
 // let's create a ROUTER
 
 import express from "express"
+import fs from "fs" // CORE MODULE => utilities for interact with file system 
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 
 const authorsRouter = express.Router()
+// i need the path of my file: first file path (c:/../src/services/authors/index.js) => i need to import fileURLToPath
+// then the folder path (c:/../src/services/authors) => import Dirname for give back the directory
+// then i have to concatenate folder path with posts.json (c:/../src/services/authors/posts.json)
+const currentFilePath = fileURLToPath(import.meta.url) // way that node use internally to indentify path and convert in url
+const currentDirPath = dirname(currentFilePath)
+// then i have to concatenate them with node.js => import join from "path"
+const postsJSONFilePath = join(currentDirPath, "posts.json")
 
 // create routers for each methods (5 endpoinds with 5 methods)
+// implement with the handle function that provide the behavior 
+// it takes two parameters = request & response
 
 // POST (+ body)
-authorsRouter.post("/")
+authorsRouter.post("/", (req, res) => {
+res.send("this is the POST route")
+})
 
 // GET
-authorsRouter.get("/")
+authorsRouter.get("/", (req, res) => {
+ const postsContent = fs.readFileSync(postsJSONFilePath)  // look at the correct file and save it in a variable
+const post = JSON.parse(postsContent)
+res.send(post)
+})
 
 // GET by id
-authorsRouter.get("/")
+authorsRouter.get("/:id", (req, res) => {
+    console.log("the current id is:", id)
+    const postsContent = JSON.parse(fs.readFileSync(postsJSONFilePath))
+    const myPost = postsContent.find(p => p.id === req.params.id)
+    res.send(myPost)
+})
 
 // PUT (id + body)
-authorsRouter.put("/")
+authorsRouter.put("/:id", (req, res) => {
+
+})
 
 // DELETE (id)
-authorsRouter.delete("/")
+authorsRouter.delete("/:id", (req, res) => {
+
+})
 
 export default authorsRouter
