a07d4565d9435fb38f05b38ea91d42b083f87205
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-16 13:28:33 +0200

    up

--- src/server.js
+++ src/server.js
@@ -1,19 +1,39 @@
 import  express  from "express";
 import listEndpoints from "express-list-endpoints";
+import { badRequestErrorHandler, forbiddenErrorHandler, genericServerErrorHandler, notFoundErrorHandler } from "./errorHandlers.js";
 import authorsRouter from "./services/authors/index.js";
 
- import cors from "cors"
+// import cors from "cors"
 
 const server = express()
 
-
- 
 const port = 3001
 
- server.use(cors())
+
+// first the GLOBAL MIDDLEWARES
+// middlewares are functions with THREE PARAMETERS 
+// REQUEST, RESPONSE, NEXT
+// let's create a global middleware that console log something for each routers
+const loggerMiddleware = (req, res, next) => {
+    console.log(`Request method ${req.method} +++++ Request URL ${req.url}`)
+    next()  // don't forget the NEXT() not to get stuck in sending request :)
+}
+
+// here i can call my GLOBAL MIDDLEWARES
+// server.use(cors())
 server.use(express.json()) // for handle the body and avoid undefined 
+server.use(loggerMiddleware)
+
 // then i have to declare the endpoints
-server.use("/authors", authorsRouter) // same prefix in mine endpoints
+// ENDPOINTS
+server.use("/blogPosts", authorsRouter) // same prefix in mine endpoints
+
+// then ERROR MIDDLEWARES
+server.use(badRequestErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(genericServerErrorHandler)
+
 console.table(listEndpoints(server))
 server.listen(port, () => {
     console.log(`my first running server on port ${port}`)
