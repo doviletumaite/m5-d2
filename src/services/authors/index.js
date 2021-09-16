635df10dc3d86c69db0969204678e920e32f1eb3
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-16 13:29:45 +0200

    up

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,6 +1,6 @@
 // here i'm gonna to create the endpoints with routers (= set of endpoints that share the same PREFIX)
-// authors CRUD 
-// http://localhost:3001/posts 
+// posts CRUD 
+// http://localhost:3001/blogPosts 
 // let's create a ROUTER
 
 import express from "express"
@@ -8,6 +8,10 @@ import fs from "fs" // CORE MODULE => utilities for interact with file system
 import { fileURLToPath } from "url"
 import { dirname, join } from "path"
 import uniqid from "uniqid" // for generate unique id
+import createHttpError from "http-errors" // this is useful for deal whit error in validation list
+import { validationResult } from "express-validator"
+import { checkBlogPost, checkValidationResult } from "./validation.js"
+
 
 const authorsRouter = express.Router()
 // i need the path of my file: first file path (c:/../src/services/authors/index.js) => i need to import fileURLToPath
@@ -17,59 +21,82 @@ const currentFilePath = fileURLToPath(import.meta.url) // way that node use inte
 
 const currentDirPath = dirname(currentFilePath)
 // then i have to concatenate them with node.js => import join from "path"
-const authorsJSONFilePath = join(currentDirPath, "authors.json")
-console.log("path of my posts.json:", authorsJSONFilePath)
+const postsJSONFilePath = join(currentDirPath, "blogPosts.json")
+console.log("path of my posts.json:", postsJSONFilePath)
 // create routers for each methods (5 endpoinds with 5 methods)
 // implement with the handle function that provide the behavior 
 // it takes two parameters = request & response
 
+// implement with the handleErrors
+
 // POST (+ body)
-authorsRouter.post("/", async (req, res) => {
+authorsRouter.post("/", checkValidationResult, checkBlogPost, (req, res, next) => {  // for handle the error i have to add next as a parameter
     console.log("REQUEST BODY: ", req.body) // we want to read the body of the new post
-    // create a new post 
-   try {
-    const { name, surname, email, dateOfBirth} = req.body
-    const newAuthor = {
-        id: uniqid(),
-        name, 
-        surname, 
-        email, 
-        dateOfBirth, 
-        avatar: `http://ui-avatars.com/api/?name=${name}+${surname}`,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        }
-    console.log("my new post", newAuthor)
-    const postsContent = JSON.parse(fs.readFileSync(authorsJSONFilePath)) // grab the array of posts
-    postsContent.push(newAuthor) // push the new post in my array
-    fs.writeFileSync(authorsJSONFilePath, JSON.stringify(postsContent))
-    res.send(newAuthor) // set two new properties of the body of my new post
-   } catch (error) {
-       res.send(500).send({message: error.message})
+   // implement with the validation sistem 
+//    const errorList = validationResult(req)
+   // add if statement to check if the error list is NOT empty
+   // if is full use the next function to create the 400 error 
+//    if (!errorList.isEmpty()){
+//        next(createHttpError(400, {errorList}))
+//    } else {
+       // if is empty (no validation error) then go forward :)
+
+       // implement the function whit try&catch method to handle errors
+       try {
+           const newPost = {
+                id: uniqid(),
+                ...req.body,
+               
+                 createdAt: new Date(),
+                 updatedAt: new Date(),
+                 } // create a new post the new post + VALIDATION
+       console.log("my new post", newPost)
+       const postsContent = JSON.parse(fs.readFileSync(postsJSONFilePath)) // grab the array of posts
+       postsContent.push(newPost) // push the new post in my array
+       fs.writeFileSync(postsJSONFilePath, JSON.stringify(postsContent))
+       res.status(201).send(newPost) // set two new properties of the body of my new post
+       } catch (error) {
+           next(error).send(500).send({ message: error.message }); 
+       }
+       
    }
-})
+)
+
 
 // GET
-authorsRouter.get("/", (req, res) => {
- const postsContent = fs.readFileSync(authorsJSONFilePath)  // look at the correct file and save it in a variable
+authorsRouter.get("/", (req, res, next) => {
+    try {
+        const postsContent = fs.readFileSync(postsJSONFilePath)  // look at the correct file and save it in a variable
 const posts = JSON.parse(postsContent) // "translate" it in json
 res.send(posts) // send back array of posts
+    } catch (error) {
+        next(error)
+    }
 })
 
+                      
+
 // GET by id
-authorsRouter.get("/:id", (req, res) => {
+authorsRouter.get("/:id", (req, res, next) => {
     console.log(" my :id", req.params.id)
-    const postsContent = JSON.parse(fs.readFileSync(authorsJSONFilePath)) // take the array and translate it 
+   try {
+    const postsContent = JSON.parse(fs.readFileSync(postsJSONFilePath)) // take the array and translate it 
     // console.log("this is the content of posts.json", postsContent )
-    console.log("check posts.json path again", authorsJSONFilePath)
+    console.log("check posts.json path again", postsJSONFilePath)
      const myPost = postsContent.find(p => p.id === req.params.id)
      console.log("body of my post", myPost)
     res.send(myPost)
+   } catch (error) {
+    next(error)
+   }
 })
 
+
+
 // PUT (id + body)
-authorsRouter.put("/:id", (req, res) => {
-    const postsContent = JSON.parse(fs.readFileSync(authorsJSONFilePath))
+authorsRouter.put("/:id", (req, res, next) => {
+    try {
+        const postsContent = JSON.parse(fs.readFileSync(postsJSONFilePath))
     const remainingPost = postsContent.filter(post => post.id !== req.params.id) // all the post except post that i'm looking for based on the id
     const updatedPost = { ...req.body, id: req.params.id} // take whatewer is in the body of my new post based on id
     remainingPost.push(updatedPost) // push the new post back in the array 
@@ -77,15 +104,25 @@ authorsRouter.put("/:id", (req, res) => {
     // const updatedPost = { ...postsContent[index], ...req.body }
     //  postsContent[index] = updatedPost
     console.log("updated post:",updatedPost )
+    fs.writeFileSync(postsJSONFilePath, JSON.stringify(postsContent)) // rewrite the array whit the updated post
     res.send(updatedPost) // send back the updated post
+    } catch (error) {
+        next(error)
+    }
 })
 
+
+
 // DELETE (id)
-authorsRouter.delete("/:id", (req, res) => {
-    const postsContent = JSON.parse(fs.readFileSync(authorsJSONFilePath)) // grab the array 
+authorsRouter.delete("/:id", (req, res, next) => {
+   try {
+    const postsContent = JSON.parse(fs.readFileSync(postsJSONFilePath)) // grab the array 
     const remainingPost = postsContent.filter(post => post.id !== req.params.id) // grab everything except the post.id that we want to delete
-    fs.writeFileSync(authorsJSONFilePath, JSON.stringify(remainingPost)) // write the remaining array of posts
+    fs.writeFileSync(postsJSONFilePath, JSON.stringify(remainingPost)) // write the remaining array of posts
     res.send() // we don't have to send nothing back because we're just deleting :)
+   } catch (error) {
+    next(error)
+   }
 })
 
 export default authorsRouter
