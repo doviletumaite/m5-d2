e41ca851a8752466f82cfc111816cb03bc983df9
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-17 08:12:14 +0200

    up

--- src/tools/fs-tools.js
+++ src/tools/fs-tools.js
@@ -1,11 +1,16 @@
 import fs from "fs-extra"
 import { join, dirname } from "path"
-// import { fileURLToPath } from "url"
+ import { fileURLToPath } from "url"
 
 
 const { writeFile } = fs
 
 // const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
+const currentFilePath = fileURLToPath(import.meta.url)
+const currentDirPath = dirname(currentFilePath)
 
- const publicFolderPath = join(process.cwd(), "./public/img")
+const publicJSONFilePath = join(currentDirPath, "img.png")
+
+ const publicFolderPath = join(publicJSONFilePath, "../../public/img")
+ console.log("path of my public:", publicFolderPath )
  export const savePicture = (name, contentAsBuffer) => writeFile(join(publicFolderPath, name), contentAsBuffer)
